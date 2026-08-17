c636541e57d53a62da0cbc346640b6a8d6aa9e7f
Zeba Shaikh <21zzshaikh11023@gmail.com>
2026-08-17 22:42:23 +0530

    Update p3.c
    added the condtionals to check for pass/fail

--- p3.c
+++ p3.c
@@ -19,12 +19,31 @@ int main()
         scanf("%d", &marks[i]);
     }
 
-    float avg = (marks[0]+marks[1]+marks[2]+marks[3]+marks[4])/5.0;
+   float avg = (marks[0]+marks[1]+marks[2]+marks[3]+marks[4])/500.0;
+    avg = avg*100.0;
 
-    printf("The percentage = %.2f\n", avg*100);
+    printf("The percentage = %.2f\n", avg);
 
     if(avg >= 35.0)
     {
-        
+        printf("Pass!\n");
+        //grades if - else if ladder
     }
+    else
+    {
+        printf("Fail :(\n");
+    }
+    return 0;
+
 }
+
+/*
+./p3.out
+Enter Subject 1 marks: 89
+Enter Subject 2 marks: 67
+Enter Subject 3 marks: 67
+Enter Subject 4 marks: 98
+Enter Subject 5 marks: 78
+The percentage = 79.80
+Pass!
+*/
