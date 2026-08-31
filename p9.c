5e0b609580e1b0ddc9cd22b874b2eecbd0272dc1
Zeba Shaikh <21zzshaikh11023@gmail.com>
2026-08-31 20:39:47 +0530

    reversing success, palindrome check added
    by this time everybody knows i don't think before doing

--- p9.c
+++ p9.c
@@ -7,27 +7,47 @@ Check whether a number reads the same reversed, reusing the digit-reversal idea.
 */
 
 #include <stdio.h>
-#include <math.h>
+//#include <math.h>
+int palindrome(int n, int rev)
+{
+    if(n==rev)
+    {
+        printf("Is a palindrome\n");
+        return 1;
+    }
+    else
+    {
+        printf("Is not a palindrome\n");
+        return 0;
+    }
+}
 
 int reverse(int n)
 {
     int rev=0, temp=n;
     
-    for(int i=0;;i++)
+    // for(int i=0;;i++)
+    // {
+    //     if(temp > 0)
+    //     {
+    //         int rem = temp%10;
+    //         rev += pow(10, i)*rem;
+    //         temp = temp/10;
+    //     }
+    //     else{
+    //         break;
+    //     }
+    // }
+
+    for(int i=0;temp>0;i++)
     {
-        if(temp > 0)
-        {
-            int rem = temp%10;
-            rev += pow(10, i)*rem;
-            temp = temp/10;
-        }
-        else{
-            break;
-        }
+        int rem = temp % 10;
+        rev = rev*10 + rem;
+        temp = temp/10;
     }
 
     printf("Reversed number: %d\n", rev);
-
+    palindrome(n, rev);
     return rev;
 }
 
@@ -37,8 +57,13 @@ int main()
 
     printf("enter a number: ");
     scanf("%d", &n);
-
-    reverse(n);
-
+    if(n<0)
+    {
+        reverse(-1*n);
+    }
+    else
+    {
+        reverse(n);
+    }
     return 0;
 }
