406688e1a300d47a9700817d747076668f30987b
Zeba Shaikh <21zzshaikh11023@gmail.com>
2026-08-31 23:13:37 +0530

    Add files via upload

--- p10.c
+++ p10.c
@@ -3,3 +3,41 @@ Harmonic Sum
 Compute 1 + 1/2 + 1/3 + … + 1/n. 
 Watch what happens if the division is done with integers.
 */
+
+#include <stdio.h>
+
+void harmonic_floating(int n)
+{
+    float hmf = 0.0;
+
+    for(float i=1; i<=(float)n; i++)
+    {
+        hmf = hmf + 1.0/i;
+    }
+    printf("Harmonic Mean with floating point variables = %f\n", hmf);
+}
+
+int main()
+{
+    int n = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int hm = 0;
+
+    for(int i=1; i<=n; i++)
+    {
+        hm = hm + 1/i;
+    }
+    printf("Harmonic Mean with integer variables = %d\n", hm);
+
+    printf("___________________________\n\n");
+
+    for(int j=0; j<=100; j++)
+    {
+        printf("J = %d\n", j);
+        harmonic_floating(j);
+    }
+    return 0;
+}
