6d7ea1a900ff82cf9582130227873829b4d3f07e
Zeba Shaikh <21zzshaikh11023@gmail.com>
2026-09-02 19:53:26 +0530

    Add files via upload

---
+++ p11.c
@@ -0,0 +1,19 @@
+/*
+Doubling Series
+Compute 1 + 2 + 4 + … + 2ⁿ. 
+Print each partial sum and notice how fast it grows — 
+and where your integer type overflows.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n=0;
+    printf("Enter n:: ");
+    scanf("%d", &n);
+
+    int dbl = 0;
+
+    // for(int i=1; i<)
+}
---
+++ p12.c
@@ -0,0 +1,5 @@
+/*
+GCD & LCM
+Compute the GCD of two numbers (Euclid's remainder 
+method encouraged) and derive the LCM from it.
+*/
